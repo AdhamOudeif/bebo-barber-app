90b50de91caca95647377b18fb3ef9441209640b
Adham Oudeif <aoudeif5@emich.edu>
2024-11-17 23:32:45 -0500

    modified git igore

--- bebo_backend/barber_app/admin.py
+++ bebo_backend/barber_app/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
+# Use this file to manage models in Django Admin Panel
+# http://127.0.0.1:8000/admin/
 
-# Register your models here.
+from django.contrib import admin
+from .models import User
+
+class UserAdmin(admin.ModelAdmin):
+    # Fields to display in the admin list view
+    list_display = ('name', 'email', 'role', 'is_verified', 'created_at')
+    
+    # Fields to filter by
+    list_filter = ('role', 'is_verified')
+    
+    # Searchable fields
+    search_fields = ('name', 'email', 'phone_number')
+
+# Register the User model with the custom admin
+admin.site.register(User, UserAdmin)
+
