a7641e8a010bfd8caa050a18b6d68ee0b95fa47c
Zack Oudeif <zacko@umich.edu>
2024-11-25 17:33:39 -0500

    Initial Flutter project setup

--- bebo_backend/bebo_backend/settings.py
+++ bebo_backend/bebo_backend/settings.py
@@ -77,17 +77,11 @@ WSGI_APPLICATION = 'bebo_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # Use the MySQL database engine
-        'NAME': 'bebo_barber_db',          # Replace with your actual database name
-        'USER': 'root',          # Replace with your actual database username
-        'PASSWORD': '',  # Replace with your actual database password
-        'HOST': 'localhost',                   # Use 'localhost' if the database is on the same machine
-        'PORT': '3306',                        # Replace with your actual database port (MySQL default is 3306)
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',  # Use the SQLite database engine
+        'NAME': BASE_DIR / 'db.sqlite3',         # Path to the SQLite database file
     }
 }
+
 
 
 # Password validation
